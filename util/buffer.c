a40655aa78dff2aaab64f5a45f30b8af3aed29ae
Nicholas Mertin <nickmertin@gmail.com>
2016-09-27 15:09:29 -0400

    Worked on buffer

---
+++ util/buffer.c
@@ -0,0 +1,5 @@
+//
+// Created by nmertin on 27/09/16.
+//
+
+#include "buffer.h"
---
+++ util/buffer.h
@@ -0,0 +1,8 @@
+//
+// Created by nmertin on 27/09/16.
+//
+
+#ifndef ISU_BUFFER_H
+#define ISU_BUFFER_H
+
+#endif //ISU_BUFFER_H
