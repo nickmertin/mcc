2297584400b0b8f8fcefa88e0abcf716ce5ad8d8
Nicholas Mertin <nickmertin@gmail.com>
2016-10-20 19:57:23 -0400

    Added swap, getFlag, and setFlag

--- util/misc.c
+++ util/misc.c
@@ -9,3 +9,14 @@ char *strdup(const char *string) {
     strcpy(r, string);
     return r;
 }
+
+void setFlag(void *data, size_t flag, bool value) {
+    char *f = (char *) data + flag / 8;
+    *f &= ~(1 << (flag % 8));
+    if (value)
+        *f |= 1 << (flag % 8);
+}
+
+bool getFlag(void *data, size_t flag) {
+    return (bool) (((char *) data)[flag / 8] & (1 << (flag % 8)));
+}
--- util/misc.h
+++ util/misc.h
@@ -1,8 +1,16 @@
 #ifndef ISU_MISC_H
 #define ISU_MISC_H
 
+#include <stdbool.h>
+
 #define isalnum_(c) (isalnum(c) || c == '_')
 
+#define swap(x, y) { x ^= y; y ^= x; x ^= y;}
+
 char *strdup(const char *string);
+
+void setFlag(void *data, size_t flag, bool value);
+
+bool getFlag(void *data, size_t flag);
 
 #endif //ISU_MISC_H
