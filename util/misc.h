2f694733deb611b4efcb55ea11df08947ca0159a
Nicholas Mertin <nickmertin@gmail.com>
2016-10-27 08:31:35 -0400

    Fixed possible issues in misc.h

--- util/misc.h
+++ util/misc.h
@@ -3,15 +3,15 @@
 
 #include <stdbool.h>
 
-#define isalnum_(c) (isalnum(c) || c == '_')
+#define isalnum_(c) (isalnum(c) || (c) == '_')
 
-#define swap(x, y) { x ^= y; y ^= x; x ^= y;}
+#define swap(x, y) ((x) ^= (y), (y) ^= (x), (x) ^= (y))
 
-#define max(x, y) (x > y ? x : y)
+#define max(x, y) ((x) > (y) ? (x) : (y))
 
-#define min(x, y) (x > y ? y : x)
+#define min(x, y) ((x) > (y) ? (y) : (x))
 
-#define sgn(x) ((x > 0) - (0 > x))
+#define sgn(x) (((x) > 0) - (0 > (x)))
 
 char *strdup(const char *string);
 
